94cae64bee46d0b800d7c96d6a5dccf9ae781c16
HilalGedik <hilalgedik0@gmail.com>
2023-01-31 23:58:29 +0300

    Add project files.

---
+++ FibonacciAverage/FibonacciFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciAverage
+{
+    public class FibonacciFunctions
+    {
+        public static int GetAverage(int n)
+        {
+            int ort;
+            int toplam = 0;
+
+            for (int i = 0; i < n-1; i++)
+            {
+                if (i !=0)
+                {
+                    toplam = toplam + ((i - 1) + (i));
+                }
+            }
+
+            ort = toplam / n;
+
+            return ort;
+        }
+    }
+}
---
+++ FibonacciAverage/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FibonacciAverage;
+
+
+
+Console.WriteLine("Fibonacci serisinin derinlik değerini giriniz.");
+int userInput = Convert.ToInt32(Console.ReadLine());
+int result = FibonacciFunctions.GetAverage(userInput);
+Console.WriteLine("Fibonacci serisinin ortalaması: " + result);
+Console.ReadLine();
