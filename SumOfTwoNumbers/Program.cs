da9f7f47d7bf60e49f922277b9a2372825ee9cfc
HilalGedik <hilalgedik0@gmail.com>
2023-02-05 23:35:38 +0300

    Added SumOfTwoNumbers project

---
+++ SumOfTwoNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Düzenlenmesini istediğiniz sayı dizinini yazınız.");
+
+
+Console.Write("Dizinin boyutunu girin: ");
+int size = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Dizinin {0}. elemanını girin: ", i + 1);
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+double[] result = SumOfTwoNumbers.SumOfTwoNumbers.GetSum(numbers);
+Console.WriteLine("Girilen dizi: " + string.Join(", ", result));
---
+++ SumOfTwoNumbers/SumOfTwoNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfTwoNumbers
+{
+    public class SumOfTwoNumbers
+    {
+        public static double[] GetSum(int[] sInts)
+        {
+            double value;
+            double sum;
+            double[] results = new double[sInts.Length/2];
+            int j = 0;
+            for (int i = 0; i < sInts.Length - 1; i=i+2)
+            {
+                if (i != sInts.Length - 1)
+                {
+                    if (sInts[i] == sInts[i + 1])
+                    {
+                        sum = sInts[i] + sInts[i + 1];
+                        value = sum * sum;
+                        results[j] = value;
+                        j = j + 1;
+                    }
+                    else if (sInts[i] != sInts[i + 1])
+                    {
+                        sum = sInts[i] + sInts[i + 1];
+                        results[j] = sum;
+                        j = j + 1;
+                    }
+                }
+                else if (i == sInts.Length-1)
+                {
+                    //if ((sInts.Length%2)!=0)
+                    //{
+                    //    results[j] = Convert.ToDouble(sInts[i]);
+                    //}
+                }
+            }
+            return results;
+        }
+    }
+}
