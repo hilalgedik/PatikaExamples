f53f8e48c9dc5b70c19154dcfaa35a3a4d4117b9
HilalGedik <hilalgedik0@gmail.com>
2023-02-05 19:30:00 +0300

    Added Algorithm project

---
+++ Algorithm/Algorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class Algorithm
+    {
+        public static string ReturnValue(string input)
+        {
+            int index;
+            int numb1;
+            int numb2;
+
+            input = input.Replace(" ", "");
+            numb1 = input.IndexOf(",");
+            numb2 = Convert.ToInt32(input.Substring(numb1+1));
+            if (numb2> input.Length)
+            {
+                Console.WriteLine("Verilen sayı bölünecek ifadenin uzunluğundan büyük olamaz!");
+                Console.ReadKey();
+            }
+            //input=input.Remove(numb2);
+           
+            StringBuilder stringBuilder = new StringBuilder(input);
+
+            stringBuilder.Remove(numb2, 1);
+            stringBuilder.Remove((numb1-1), input.Length - (numb1));
+
+           return  stringBuilder.ToString();
+
+
+            
+
+
+        }
+    }
+}
---
+++ Algorithm/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Lütfen bir bölünmesini istediğiniz değeri ve değerin indexini girin, Ör: 'xxx,2' ");
+string input = Console.ReadLine();
+input = Algorithm.Algorithm.ReturnValue(input);
+Console.WriteLine(input);
