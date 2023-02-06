de9bfffa3e622a73f9b04a7a448dbc8210f385a5
HilalGedik <hilalgedik0@gmail.com>
2023-02-06 23:53:07 +0300

    Added ChangeCharacter project

---
+++ ChangeCharacter/ChangeCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeCharacter
+{
+    public class ChangeCharacter
+    {
+        public static StringBuilder GetNewValue()
+        {
+            Console.WriteLine("İşlem yapılmasını istediğiniz kelimeyi giriniz.");
+            string input = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder(input);
+
+           // sb = sb.Replace(sb[0], sb[sb.Length - 1]);
+
+            sb[0] = sb[sb.Length - 1];
+            sb[sb.Length - 1] = input[0];
+
+
+
+
+            return sb;
+        }
+    }
+}
---
+++ ChangeCharacter/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+string input = ChangeCharacter.ChangeCharacter.GetNewValue().ToString();
+Console.WriteLine(input);
